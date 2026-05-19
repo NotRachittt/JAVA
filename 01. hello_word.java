5bfcdf566d7c085485f21638d1666708480791c9
Rachit Shrirame <shriramerachit@gmail.com>
2026-05-19 17:28:51 +0530

    Add files via upload

---
+++ 01. hello_word.java
@@ -0,0 +1,8 @@
+
+public class hello_word {  //This is main class major code is inside this
+
+	public static void main(String[] args) {  //this is main function
+		System.out.println("Hello World");	  //Methods is that function which is written inside classes
+			}
+
+}
