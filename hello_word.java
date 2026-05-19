1cbf320c73c8c2d9081db8f1d71aa44d3583f794
Rachit Shrirame <shriramerachit@gmail.com>
2026-05-19 17:19:24 +0530

    Delete hello_word.java

--- hello_word.java
+++
@@ -1,8 +0,0 @@
-
-public class hello_word {  //This is main class major code is inside this
-
-	public static void main(String[] args) {  //this is main function
-		System.out.println("Hello World");	  //Methods is that function which is written inside classes
-			}
-
-}
